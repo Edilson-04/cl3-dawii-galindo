940fed2e0c92c887e5ca3a4278c7eedb14385084
Edilson-04 <edilsdondelpiero@gmail.com>
2023-11-25 20:00:15 -0500

    preguntados

--- src/app/home/pregunta-dos/pregunta-dos.component.ts
+++ src/app/home/pregunta-dos/pregunta-dos.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class PreguntaDosComponent {
 
+
+  notas: number[] = [10, 9, 8, 7];
+  promedio: number = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  calcularPromedio() {
+    // Ordenar las notas en orden descendente
+    this.notas.sort((a, b) => b - a);
+
+    // Eliminar la nota más baja
+    this.notas.splice(this.notas.length - 1, 1);
+
+    // Multiplicar cada nota restante por su peso correspondiente
+    const notasPonderadas = this.notas.map((nota) => nota * 0.2 + nota * 0.3 + nota * 0.5);
+
+    // Sumar los resultados de las multiplicaciones
+    const suma = notasPonderadas.reduce((a, b) => a + b);
+
+    // Dividir el resultado por la cantidad de notas restantes
+    this.promedio = suma / notasPonderadas.length;
+  }
+
 }
--- src/app/home/pregunta-uno/pregunta-uno.component.ts
+++ src/app/home/pregunta-uno/pregunta-uno.component.ts
@@ -1,4 +1,16 @@
+/*
 import { Component } from '@angular/core';
+import { MatInput } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+*/
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+
+
+
 
 @Component({
   selector: 'app-pregunta-uno',
@@ -6,5 +18,22 @@ import { Component } from '@angular/core';
   styleUrl: './pregunta-uno.component.css'
 })
 export class PreguntaUnoComponent {
+
+  horas: number = 0;
+  minutos: number = 0;
+  total: number = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.total = 0;
+  }
+
+  calcularTotal() {
+    this.total = this.horas * 1500;
+    if (this.minutos > 0) {
+      this.total += (this.minutos / 60) * 1500;
+    }
+  }
 
 }
